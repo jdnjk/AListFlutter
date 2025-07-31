e0ae423aaec394ab22a7abecab9d9b21a11b7b6c
jdnjk j <2394058778@qq.com>
2025-07-31 11:32:33 +0000

    test

--- alist-lib/scripts/init_web.sh
+++ alist-lib/scripts/init_web.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -L https://github.com/alist-org/alist-web/releases/latest/download/dist.tar.gz -o dist.tar.gz
+curl -L https://github.com/OpenListTeam/OpenList-Frontend/releases/download/v4.1.0/openlist-frontend-dist-lite-v4.1.0.tar.gz -o dist.tar.gz
 tar -zxvf dist.tar.gz
 rm -rf ../public/dist
 mv -f dist ../public
