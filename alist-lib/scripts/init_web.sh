490572786a86db17bf1c5a39a20ef6fdd3b24adc
jdnjk <96813281+jdnjk@users.noreply.github.com>
2025-08-01 09:21:20 +0800

    Update init_web.sh

--- alist-lib/scripts/init_web.sh
+++ alist-lib/scripts/init_web.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 curl -L https://github.com/OpenListTeam/OpenList-Frontend/releases/download/v4.1.0/openlist-frontend-dist-lite-v4.1.0.tar.gz -o dist.tar.gz
-tar -zxvf dist.tar.gz
+rm -rf ./dist
+mkdir dist
+tar -zxvf dist.tar.gz -C ./dist --strip-components=1
 rm -rf ../public/dist
 mv -f dist ../public
 rm -rf dist.tar.gz
