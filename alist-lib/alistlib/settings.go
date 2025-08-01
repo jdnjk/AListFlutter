425ce630c0cbc02e4fa6fcff8ee193755b12f726
jdnjk <96813281+jdnjk@users.noreply.github.com>
2025-08-01 08:14:29 +0800

    更新 settings.go

--- alist-lib/alistlib/settings.go
+++ alist-lib/alistlib/settings.go
@@ -1,10 +1,10 @@
 package alistlib
 
 import (
-	"github.com/alist-org/alist/v3/cmd"
-	"github.com/alist-org/alist/v3/cmd/flags"
-	"github.com/alist-org/alist/v3/internal/op"
-	"github.com/alist-org/alist/v3/pkg/utils"
+	"github.com/OpenListTeam/OpenList/v4/cmd"
+	"github.com/OpenListTeam/OpenList/v4/cmd/flags"
+	"github.com/OpenListTeam/OpenList/v4/internal/op"
+	"github.com/OpenListTeam/OpenList/v4/pkg/utils"
 )
 
 func SetConfigData(path string) {
